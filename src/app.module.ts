12a3856eea661d9540ccc0e432b955d65993f66d
huydevct <huyvipct1@gmail.com>
2021-10-20 10:30:23 +0700

    use mongodb in nest js

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    ProductsModule,
+    MongooseModule.forRoot(
+      'mongodb+srv://huy:huy123@cluster0.zfyia.mongodb.net/demo-nestjs?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ProductsController } from './products.controller';
 import { ProductsService } from './products.service';
+import { ProductSchema } from './products.model';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Product', schema: ProductSchema }]),
+  ],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
